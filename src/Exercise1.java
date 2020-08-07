29bb4731ce2e45a3db430815046bb1c23d8f3a01
gryan20 <67970791+gryan20@users.noreply.github.com>
2020-08-06 19:09:21 -0500

    slight change
    slight change

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -5,33 +5,42 @@ import java.util.Scanner;
 public class Exercise1 {
 
 	
-	public static void main(String[] args) throws FileNotFoundException {
-	
+	public static void main(String args[]) throws FileNotFoundException {
 
 		Scanner scan = new Scanner(new File("C:\\Users\\Gabby\\Documents\\Programming Fundamentals\\input.csv"));
 		String line = "";
-		
+
 		int max = 0, count = 1;
 		String[] arr = null;
-		
-		System.out.println("Maximum values");
-		
-		while(scan.hasNext())
-		{
-			line = scan.nextLine();
-			arr = line.split(",");
-			max = 0;
-			
-			for (int i = 0; i < arr.length; i++)
-			{
-				if(Integer.parseInt(arr[i]) > max)
-				max = Integer.parseInt(arr[i]);
-			}
-		System.out.println("ROW " + count + " : " + max);
-		count++;
-		
-		scan.close();
-		}
-	}
-}
 
+		System.out.println("Maximum values");
+
+		while(scan.hasNext())
+
+		{
+
+		line = scan.nextLine();
+
+		arr = line.split(",");
+
+		max = 0;
+
+		for(int i=0; i<arr.length; i++)
+
+		{
+
+		if(Integer.parseInt(arr[i]) > max)
+
+		max = Integer.parseInt(arr[i]);
+
+		}
+
+		System.out.println("ROW " + count + " : " + max);
+
+		count++;
+
+		}
+
+		}
+
+		}
